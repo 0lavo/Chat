e9c5a1365033b16fe8af6b75c25cd4612f054d15
Olavo Gonçalves <olavo.goncalves2005@gmail.com>
2023-11-20 14:00:33 +0000

    Added client connection method

--- Chat-Project/src/main/java/Client/Client.java
+++ Chat-Project/src/main/java/Client/Client.java
@@ -1,4 +1,28 @@
 package Client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+    private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    private Socket socket;
+
+    public void init() {
+        try {
+
+            bufferedWriter.write("What is the inetAddress of the server ? \n");
+            bufferedWriter.flush();
+            String inetAddress = bufferedReader.readLine();
+
+            bufferedWriter.write("What is the port of the server ? \n");
+            bufferedWriter.flush();
+            Integer port = Integer.valueOf(bufferedReader.readLine());
+
+            socket = new Socket(inetAddress,port);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
