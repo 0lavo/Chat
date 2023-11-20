65ed5f6d6eb945e1e7e86a72b3a1a689d9599504
Olavo Gonçalves <olavo.goncalves2005@gmail.com>
2023-11-20 11:05:44 +0000

    added Server and ServerWorker dynamics

--- Chat-Project/src/Server/Server.java
+++ Chat-Project/src/Server/Server.java
@@ -1,4 +1,102 @@
 package Server;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    public final static int port = 8000;
+    private Socket clientSocket;
+    private ServerSocket serverSocket;
+    private boolean serverOn = false;
+    private ServerWorker serverWorker;
+    private ArrayList<ServerWorker> serverWorkerArrayList;
+    private ExecutorService fixedPool = Executors.newFixedThreadPool(30);
+    private BufferedWriter bufferedWriter;
+
+    public void powerOn() {
+        serverOn = true;
+        try {
+            serverSocket = new ServerSocket(port);
+            while (serverOn) {
+                clientSocket = serverSocket.accept();
+                serverWorker = new ServerWorker(clientSocket, this);
+                serverWorkerArrayList.add(serverWorker);
+                fixedPool.submit(serverWorker);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void messageTreatment(String message, Socket socket) {
+        switch (message) {
+            case "/quit":
+                finishConnection(socket);
+                break;
+            case "/list":
+                listClients(socket);
+                break;
+            default:
+                sendMessageAll(message, socket);
+                break;
+        }
+    }
+    private void finishConnection(Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void listClients(Socket socket) {
+
+        int numberOfClients = 1;
+
+        try {
+
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("---------------");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (ServerWorker s : serverWorkerArrayList) {
+            if (s.getSocket() == socket) {
+                continue;
+            }
+
+            try {
+
+                bufferedWriter.write(numberOfClients + " - " + s.getClientName());
+                numberOfClients++;
+
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        try {
+            bufferedWriter.write("---------------");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void sendMessageAll(String message, Socket socket) {
+
+        for (ServerWorker s : serverWorkerArrayList) {
+            try {
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(s.getSocket().getOutputStream()));
+                bufferedWriter.write(s.getClientName() + ": " + message);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- Chat-Project/src/Server/ServerWorker.java
+++ Chat-Project/src/Server/ServerWorker.java
@@ -1,4 +1,41 @@
 package Server;
 
-public class ServerWorker {
+import java.io.*;
+import java.net.Socket;
+
+public class ServerWorker implements Runnable{
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private Server server;
+    private String clientName;
+    private BufferedWriter bufferedWriter;
+
+    public ServerWorker(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("----- Welcome to my server what is your name ? -----");
+            clientName = bufferedReader.readLine();
+            while (!socket.isClosed()) {
+                server.messageTreatment(bufferedReader.readLine(), socket);
+            }
+
+       } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
 }
